cf57b0762e8740caa84ba12aeaee76556bab52a9
Deema Burhama <eng.dburhama@gmail.com>
2021-11-22 11:38:56 +0300

    continue

--- components/Signin.js
+++ components/Signin.js
@@ -3,9 +3,83 @@ import { StyleSheet, Text, View } from "react-native";
 
 const Signin = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Center flex={1} px="3">
+      <Box safeArea p="2" py="8" w="90%" maxW="290">
+        <Heading
+          size="lg"
+          fontWeight="600"
+          color="coolGray.800"
+          _dark={{
+            color: "warmGray.50",
+          }}
+        >
+          We make world travel easy!
+        </Heading>
+        <Heading
+          mt="1"
+          _dark={{
+            color: "warmGray.200",
+          }}
+          color="coolGray.600"
+          fontWeight="medium"
+          size="xs"
+        >
+          Talk to a travel expert to find your perfect tour{" "}
+        </Heading>
+
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>User Name</FormControl.Label>
+            <Input
+              onChangeText={(username) => setUser({ ...user, username })}
+            />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input
+              type="password"
+              onChangeText={(password) => setUser({ ...user, password })}
+            />
+
+            <Link
+              _text={{
+                fontSize: "xs",
+                fontWeight: "500",
+                color: "indigo.500",
+              }}
+              alignSelf="flex-end"
+              mt="1"
+            >
+              Forget Password?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo" onPress={handleSubmit}>
+            Sign up
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text
+              fontSize="sm"
+              color="coolGray.600"
+              _dark={{
+                color: "warmGray.200",
+              }}
+            >
+              Sign in Please{" "}
+            </Text>
+            <Link
+              _text={{
+                color: "indigo.500",
+                fontWeight: "medium",
+                fontSize: "sm",
+              }}
+              onPress={() => navigation.navigate("Signup")}
+            >
+              Sign up
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </Center>
   );
 };
 
